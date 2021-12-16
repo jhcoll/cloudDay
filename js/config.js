d8d8a156ac375b996b688bb09acf0c1177a67a3f
jcollier <jcollier@scottlogic.com>
2021-12-16 15:29:34 +0000

    changes

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_Ide1qA0sG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '67oildkiij1ffg0m7tah5t5vf7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
